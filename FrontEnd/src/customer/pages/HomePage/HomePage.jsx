932a360e9be4f3fd9a694872b15dddd6b3f36082
Guru-Dharpale <gurudharapale4334@gmail.com>
2024-06-10 15:44:51 +0530

    slideChanges

--- FrontEnd/src/customer/pages/HomePage/HomePage.jsx
+++ FrontEnd/src/customer/pages/HomePage/HomePage.jsx
@@ -10,10 +10,7 @@ const HomePage=()=>{
             <div className=" space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
                 <HomeSectionCarousal/>
                 <HomeSectionCarousal/>
-                <HomeSectionCarousal/>
-                <HomeSectionCarousal/>
-                <HomeSectionCarousal/>
-                <HomeSectionCarousal/>
+                
             </div>
         </div>
     )
