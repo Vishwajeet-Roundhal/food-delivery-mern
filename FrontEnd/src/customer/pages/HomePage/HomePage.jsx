973ea3843840d12982289d533b1010ccff2229e0
Vishwajeet Roundhal <vishwajeetroundhal0@gmail.com>
2024-06-19 18:51:32 +0530

    api

--- FrontEnd/src/customer/pages/HomePage/HomePage.jsx
+++ FrontEnd/src/customer/pages/HomePage/HomePage.jsx
@@ -4,8 +4,16 @@ import HomeSectionCarousal from "../../components/HomeSectionCarousal/HomeSectio
 import dishes from "../../../Data/Dishes"
 
 const HomePage=()=>{
+    const getRestoData = async()=>{
+        const response = await fetch("http://localhost:6005/api/restaurant/restaurants");
+        const data = await response.json();
+        console.log(data);
+        
+    }
     return(
+        
         <div>
+            
             <MainCrosel></MainCrosel>
             <div className=" space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
                 <HomeSectionCarousal data={dishes} sectionName={"Punes Best Dishes"}/ >
